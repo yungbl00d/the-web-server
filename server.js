f6441efb2c8fcb096f0b236e1c92153153489bdb
Theo Schrader <trschrader@gmail.com>
2018-06-05 23:42:32 +0900

    nice

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    console.log('cant append');
+  });
+  next();
+
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+app.get('/', (req, res) => {
+  //res.send('<h1>hello</h1>');
+  res.render('home.hbs', {
+    pageTitle: 'Home page',
+    welcomeMessage: 'what up homie'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page',
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: "fuckt up bro"
+  });
+});
+
+app.listen(3000, () => {
+  console.log('server is up baby');
+});
